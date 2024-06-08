5070e7746bd9c895b2ac693b241ed79809b26f0d
Tyler Harker <tylerharker@Tylers-MacBook-Pro.local>
2024-06-08 11:43:39 -0700

    chore: updated names

--- TemporalIO.Community.Domain.ValueObjects/obj/Debug/net8.0/TemporalIO.Community.Domain.ValueObjects.AssemblyInfo.cs
+++ TemporalIO.Community.Domain.ValueObjects/obj/Debug/net8.0/TemporalIO.Community.Domain.ValueObjects.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TemporalIO.Community.Domain.ValueObjects")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+52faadfd2d67ba1e92f0df483555b970197cbb58")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11f4f12323e7d7cf31c56c6dbc7f0f75414f6a03")]
 [assembly: System.Reflection.AssemblyProductAttribute("TemporalIO.Community.Domain.ValueObjects")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TemporalIO.Community.Domain.ValueObjects")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
